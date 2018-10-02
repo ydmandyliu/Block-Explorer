389d04fbad0d1a5165ae4fad9334b2d5c05210cf
Mandy Liu <ydmandyliu@gmail.com>
2018-10-01 17:40:28 -0700

    added javascript and css manifest files

---
+++ app/assets/javascripts/explorer.js
@@ -0,0 +1,7 @@
+// Explorer Javascript Manifest File
+//= require jquery2
+//= require bootstrap
+//= require angular
+//= require angular-ui-router
+//= require angular-resource
+//= require angular-cookie
