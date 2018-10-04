1a8aa8c8bd863408eaa637e7a82bbe05d66e559d
Mandy Liu <ydmandyliu@gmail.com>
2018-10-03 17:01:06 -0700

    declared more modules in the manifest

--- app/assets/javascripts/explorer.js
+++ app/assets/javascripts/explorer.js
@@ -4,6 +4,7 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-resource
+//= require app
 
 //= require explorer/app.module
 //= require explorer/app.router
@@ -12,3 +13,20 @@
 
 //= require explorer/layout/layout.module
 //= require explorer/layout/navbar/navbar.component
+
+//= require explorer/subjects/subjects.module
+
+//= require explorer/blocks/blocks.module
+//= require explorer/blocks/blocks.controller
+//= require explorer/blocks/blocks.directive
+//= require explorer/blocks/blocks.service
+
+//= require explorer/transactions/transactions.module
+//= require explorer/transactions/transactions.controller
+//= require explorer/transactions/transactions.directive
+//= require explorer/transactions/transactions.service
+
+//= require explorer/stats/stats.module
+//= require explorer/stats/stats.controller
+//= require explorer/stats/stats.directive
+//= require explorer/stats/stats.service
